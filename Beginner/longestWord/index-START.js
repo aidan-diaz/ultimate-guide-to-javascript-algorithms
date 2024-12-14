4c5e2d816ee6a35e03c52128c387f4578c31a2b3
Aidan Diaz <diaz.aidan.d@gmail.com>
2024-12-14 16:07:26 -0500

    completed longestWord code

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -1,13 +1,46 @@
 /* CHALLENGE
-Given a string of text, write an algorithm that returns the text received in a reversed format. 
-E.g reverseString('algorithms') // should return 'smhtirogla'
+Given a sentence, return the longest word in the string.
 */
 
+//FOR LOOP
 
+// function longestWord(text) {
+//     let longest = ''
+//     const arr = text.split(' ')
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i].length > longest.length) {
+//             longest = arr[i]
+//         }
+//     }
+//     return longest
+// }
+
+//create variable to store longest word in, initially set to empty str
+//split text into arr by spaces
+//loop through arr
+//if current word length > longest word length, update longest word to be current word
+//return longest word
+
+
+//REDUCE METHOD
+// (STUDY - DID NOT KNOW HOW TO DO)
+
+// function longestWord(text) {
+//     const result = text.split(' ').reduce((longestWord, currentWord) => {
+//         if(currentWord.length > longestWord.length) {
+//             return currentWord
+//         }else {
+//             return longestWord
+//         }
+//     }, '')
+//     return result
+// }
+
+//SORT METHOD --> 83% SLOWER THAN OTHER SOLUTIONS
 
 function longestWord(text) {
-    // Code goes here
+    const arr = text.split(' ').sort( (a, b) => b.length - a.length )
+    return arr[0]
 }
-
 
 module.exports = longestWord
