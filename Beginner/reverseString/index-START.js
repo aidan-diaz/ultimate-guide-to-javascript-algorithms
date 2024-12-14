e76f8709636e7b6b46744d999d63c81457738ba9
Aidan Diaz <diaz.aidan.d@gmail.com>
2024-12-14 00:36:25 -0500

    completed reversedStrings code

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -4,11 +4,52 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
 
+//METHODS
+// function reverseString(text) {
+//     return text.split('').reverse().join('')
+// }
 
+//split str into array
+//reverse array
+//join str back together w no delimiter
+//return new str
+
+
+//FOR LOOP
+// function reverseString(text) {
+//     let reversedStr = ''
+//     for(let i = text.length - 1; i >= 0; i--) {
+//         reversedStr += text[i]
+//     }
+//     return reversedStr
+// }
+
+//declare variable & assign '' val
+//loop through input text starting @ last index, end at 0 index
+//for each iteration, concat current char to empty str var
+//return updated var
+
+
+//RECURSION
 function reverseString(text) {
-    // Code goes here
+    if(text === '') {
+        return ''
+    }else {
+        return reverseString(text.substring(1)) + text[0]
+    }
 }
 
+//COME BACK TO THIS
+
+//REDUCE
+// function reverseString(text) {
+//     return text.split('').reduce( (acc, cur) => cur + acc, '')
+// }
+
+//use reduce method
+//split str into arr
+//set acc in empty arr to be ''
+//return reduced str
 
 
 module.exports = reverseString
