775fd0dd78f5c3a90e743c1c79f8bca41302be30
Aidan Diaz <diaz.aidan.d@gmail.com>
2024-12-14 15:52:19 -0500

    completed hammingDistance code

--- Beginner/hammingDistance/index-START.js
+++ Beginner/hammingDistance/index-START.js
@@ -4,12 +4,27 @@ E.g hammingDistance('rover', 'river') // should return 1
 */
 
 
-
+//FOR LOOP
 
 function hammingDistance(stringA, stringB) {
-    // Code goes here
+    let counter = 0
+    stringA = stringA.toLowerCase()
+    stringB = stringB.toLowerCase()
+    for(let i = 0; i < stringA.length; i++) {
+        if(stringA[i] != stringB[i]) {
+            counter += 1
+        }
+    }
+    return counter
 }
 
+//declare a counter, set to 0
+//make both strs lowercase
+//for loop to loop through each char in strs
+//if current char in str 1 doesn't equel current char in str 2, add 1 to counter
+//return counter
 
+//NOTES GOING FORWARD
+//account for edge cases where strs might not be same length
 
 module.exports = hammingDistance
