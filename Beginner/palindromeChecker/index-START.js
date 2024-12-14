209e1d350c138c710a901feaa8de65ddfdc55fd2
Aidan Diaz <diaz.aidan.d@gmail.com>
2024-12-14 15:45:42 -0500

    completed palindtomeChecker code

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -4,12 +4,34 @@ e.g palindromeChecker('racecar') // will return true
 */
 
 
+//INTUITIVE
 
+// function palindromeChecker(text) {
+//     text = text.toLowerCase()
+//     const textReversed = text.split('').reverse().join('')
+//     return text == textReversed
+// }
+
+//make input text lowercase
+//make variable for reversedText
+//set to value of text split rev join
+//return comparison of text and reversedText
+
+//LOOP & COMPARE
 
 function palindromeChecker(text) {
-    v// Code goes here
+    text = text.toLowerCase()
+    for(let i = 0; i < text.length; i++) {
+        if( text[i] != text[text.length - (i + 1)]) {
+            return false
+        }
+    }
+    return true
 }
 
-
+//make text lowercase
+//make for loop
+//if text[i] != text[text.length - (i + 1)] return false
+//return true outside of loop
 
 module.exports = palindromeChecker;
