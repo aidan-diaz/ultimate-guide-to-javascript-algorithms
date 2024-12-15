5b058138576c78fb0647de479624501a0131b4c5
Aidan Diaz <diaz.aidan.d@gmail.com>
2024-12-15 15:31:03 -0500

    completed mergeArrays code

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -4,18 +4,60 @@
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+//MY SOLUTION WITHOUT LOOKING AT OTHER SOLUTIONS
+
 function mergeArrays(...arrays) {
 
-    let jointArray = []
-    
-    arrays.forEach(array => {
-        jointArray = [...jointArray, ...array]
-    });
-
-    return [...new Set([...jointArray])]
-
-    
+    const removedDuplicatesArr = Array.from( new Set( arrays.flat() ) )
+    return removedDuplicatesArr
 }
 
+//flatten input arrays into one array
+//make new set from that array
+//turn set back into array and return
+
+
+//SPREAD OPERATOR AND SETS --> SECOND FASTEST - 33% SLOWER THAN FILTER
+
+// function mergeArrays(...arrays) {
+//     let jointArray = []
+
+//     arrays.forEach(array => {
+//         jointArray = [...jointArray, ...array]
+//     });
+//     return [...new Set([...jointArray])]
+// }
+
+
+//FILTER METHOD --> FASTEST
+
+// function mergeArrays(...arrays) {
+//     let jointArray = []
+
+//     arrays.forEach(array => {
+//         jointArray = [...jointArray, ...array]
+//     })
+//     const uniqueArray = jointArray.filter((item,index) => jointArray.indexOf(item) === index)
+//     return uniqueArray
+// }
+
+
+//REDUCE
+
+// function mergeArrays(...arrays) {
+//     let jointArray = []
+
+//     arrays.forEach(array => {
+//         jointArray = [...jointArray, ...array]
+//     })
+//     const uniqueArray = jointArray.reduce((newArray, item) =>{
+//         if (newArray.includes(item)){
+//             return newArray
+//         } else {
+//             return [...newArray, item]
+//         }
+//     }, [])
+//     return uniqueArray
+// }
 
 module.exports = mergeArrays
