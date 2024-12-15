025f41d7aabacebd9f4db4ed10d7dfeacfe2ecff
Aidan Diaz <diaz.aidan.d@gmail.com>
2024-12-15 16:55:39 -0500

    completed factorial code

--- Beginner/factorial/index-START.js
+++ Beginner/factorial/index-START.js
@@ -4,9 +4,21 @@ the provided integer(n). E.g
     factorial(5) // should return 120
 */
 
+//MY SOLUTION
 
 function factorial(n) {
-
+    let product = 1
+    while(n > 0) {
+        product *= n
+        n--
+    }
+    return product
 }
+
+//create product variable, set to 1
+//run while loop
+//keep multiplying current num by product
+//after each multiply, set n to be n - 1
+//return product
 
 module.exports = factorial
