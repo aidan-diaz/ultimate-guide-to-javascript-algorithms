0114e7711d98b5c9c37fb0d5cf0b8ad8f29416ac
Aidan Diaz <diaz.aidan.d@gmail.com>
2024-12-15 16:11:47 -0500

    completed whereIBelong code

--- Beginner/whereIBelong/index-START.js
+++ Beginner/whereIBelong/index-START.js
@@ -7,13 +7,87 @@ whereIBelong([1,2,3,4], 1.5) // should return 1 because it is greater
 than 1(index 0), but less than 2(index 1).
 */
 
-
+//MY SOLUTION ATTEMPT WITHOUT LOOKING AT OTHER SOLUTIONS
 
 function whereIBelong(arr, num) {
-   // Code goes here
+   if(arr == null || !arr.length) {
+      return 0
+   }
+   arr = arr.sort( (a, b) => a - b )
+
+   if(num < arr[0]) {
+      return 0
+   }else if(num > arr[arr.length - 1]) {
+      return arr.length
+   }
+
+   for(let i = 0; i < arr.length; i++) {
+      if(num > arr[i] && num < arr[i + 1]) {
+         return i + 1
+      }else if(num == arr[i]) {
+         return i
+      }
+   }
 
 }
 
+//if no arr or empty, return 0
+//sort arr from low to high
+//for loop
+//check num against all nums in arr
+//if num > current num and < next num, return current index
+//if num > all num, return last index
+//if num < all num, return 0
+
+
+//FOR LOOP
+
+// function whereIBelong(arr, num) {
+//    arr.sort((a, b) => {
+//        return a - b
+//    })
+//    for (var i = 0; i < arr.length; i++) {
+//        if (arr[i] >= num) {            
+            // return i
+//        }
+//    }
+//    return arr.length
+// }
+
+//FOR LOOP #2 - COUNT SMALLER VALUES --> AT LEAST 90% FASTER THAN OTHER SOLUTIONS
+
+// function whereIBelong(arr, num) {
+//    var counter = 0
+//    for (i = 0; i < arr.length; i++) {
+//        if (num > arr++[++i]) {
+//            counter++;
+//        }
+//    } 
+//    return counter
+// }
+
+
+//WHILE LOOP
+
+// function whereIBelong(arr, num) {
+//    arr.sort((a, b) => {
+//        return a - b
+//    })
+//    let counter = 0;
+//    while (num > arr[counter]) {
+//        counter++
+//    }
+//    return counter
+// }
+
+
+//PUSH, SORT, FIND INDEX
+
+// function whereIBelong(arr, num) {
+//    arr.push(num)
+//    arr.sort((a, b) => a - b)
+//    return arr.indexOf(num)
+// }
 
 
 module.exports = whereIBelong
